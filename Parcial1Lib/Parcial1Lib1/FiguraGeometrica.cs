74ff08857e99d7def88a0ece784f980953b3fd3d
auza samuel <auzasamu10@gmail.com>
2025-04-07 20:34:31 -0400

    Primer parcial

---
+++ Parcial1Lib/Parcial1Lib1/FiguraGeometrica.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial1Lib1
+{
+    public abstract class FiguraGeometrica
+    {
+        public abstract decimal area();
+
+        public abstract decimal perimetro();
+    }
+    public class Circulo : FiguraGeometrica
+    {
+        private readonly decimal radio;
+        public Circulo(decimal radio)
+        {
+            this.radio = radio;
+        }
+        public override decimal area()
+        {
+            return (decimal)Math.PI * radio * radio;
+        }
+        public override decimal perimetro()
+        {
+            return 2 * (decimal)Math.PI * radio;
+        }
+    }
+    public class Triangulo : FiguraGeometrica
+    {
+        private readonly decimal l1;
+        private readonly decimal l2;
+        private readonly decimal baseTriangulo;
+        private readonly decimal altura;
+        public Triangulo(decimal l1, decimal l2, decimal altura, decimal baseTriangulo)
+        {
+            this.l1 = l1;
+            this.l2 = l2;
+            this.altura = altura;
+            this.baseTriangulo = baseTriangulo;
+        }
+        public override decimal area()
+        {
+            return (baseTriangulo * altura) / 2;
+        }
+        public override decimal perimetro()
+        {
+            return l1 + l2 + baseTriangulo;
+        }
+    }
+
+}
---
+++ Parcial1Lib/Parcial1Test/TestUnitTriangulo.cs
@@ -0,0 +1,29 @@
+﻿
+using Parcial1Lib1;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Parcial1Test
+{
+    [TestClass]
+    public sealed class TesUnitTriangulo
+    {
+        [TestMethod]
+        public void Triangulo()
+        {
+            decimal l1 = 3;
+            decimal l2 = 4;
+            decimal baseTriangulo = 5;
+            decimal altura = 6;
+            Triangulo triangulo = new Triangulo(l1, l2, altura, baseTriangulo);
+            decimal area = triangulo.area();
+            decimal perimetro = triangulo.perimetro();
+            Assert.AreEqual(15, area);
+            Assert.AreEqual(12, perimetro);
+        }
+    }
+}
+
